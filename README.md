4bcce563cdb96415a0b37fb3efcd9e411d5d4d1b
Joao Paulo Cordio <joao.cordio@gmail.com>
2024-05-31 22:34:37 +0100

    Updating Script file and Adding readme file

---
+++ README.md
@@ -0,0 +1,3 @@
+# Week 2 Lab – Bash & Git
+Joao Cordio - x23309644
+[My GitHub Page](https://github.com/jpcordio)
--- script.sh
+++ script.sh
@@ -1,19 +1,26 @@
- #!/bin/bash
+#!/bin/bash
 
 rm -r fol_1
 rm -r fol_2
+echo "# Week 2 Lab – Bash & Git" > README.md
+echo "Joao Cordio - x23309644" >> README.md
+echo "[My GitHub Page](https://github.com/jpcordio)" >> README.md
 
 mkdir fol_1
 cd fol_1
 touch 1_1.txt
 touch 1_2.txt
 touch 1_3.txt
+chmod 600 1_1.txt 1_3.txt
+chmod 700 1_2.txt
 cd ../
 mkdir fol_2
 cd fol_2
 touch 2_1.txt
 touch 2_2.txt
 touch 2_3.txt
+chmod 600 2_1.txt 2_3.txt
+chmod 700 2_2.txt
 cd ../
 touch .gitignore
 echo fol_1/ > .gitignore
