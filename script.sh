9d36be92adbfca1b2644ecadc4790fd382930ec4
Joao Paulo Cordio <joao.cordio@gmail.com>
2024-05-31 21:32:51 +0100

    First Commit

---
+++ script.sh
@@ -0,0 +1,17 @@
+ #!/bin/bash
+
+mkdir fol_1
+cd fol_1
+touch 1_1.txt
+touch 1_2.txt
+touch 1_3.txt
+cd ../
+mkdir fol_2
+cd fol_2
+touch 2_1.txt
+touch 2_2.txt
+touch 2_3.txt
+cd ../
+touch .gitignore
+echo fol_1/ > .gitignore
+echo fol_2/ >> .gitignore
